ef31640b01bec2ce7f05d29cb891b4946be57720
Aleksey Kish <ttlaleksey@gamil.com>
2024-05-19 17:35:02 +0300

    new_script.sh for dig

---
+++ new_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "введите домен: " www
+echo A
+dig $www +short
+echo MX
+dig $www mx +short
+echo NS
+dig $www ns +short
+echo Mail-server
+dig mail.$www +short
+echo TXT
+dig $www txt +short
